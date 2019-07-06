75f4c58e98a3e464b1af221d4c41c41edd181d17
tyc <952530904@qq,com>
2019-07-06 10:36:22 +0800

    mysql服务器切换到万家明的云主机

--- db/src/main/java/com/pyteam/db/mapper/TestMapper.java
+++ db/src/main/java/com/pyteam/db/mapper/TestMapper.java
@@ -1,21 +1,22 @@
 package com.pyteam.db.mapper;
 
-// import com.pyteam.db.entity.Test;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import com.pyteam.db.entity.Test;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author tyc
  * @date 2019/7/1 10:54
  */
-// @Mapper
-// public interface TestMapper
-// {
-//     @Select("select * from test where name = #{name}")
-//     Test findByName(@Param("name") String name);
-//
-//     @Insert("insert into test(name, age) values(#{name}, #{age})")
-//     int insert(@Param("name") String name, @Param("age") Integer age);
-// }
+@Repository
+public interface TestMapper
+{
+    @Select("select * from test where name = #{name}")
+    Test findByName(@Param("name") String name);
+
+    @Insert("insert into test(name, age) values(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+}
--- db/src/test/java/com/pyteam/db/DbApplicationTests.java
+++ db/src/test/java/com/pyteam/db/DbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.pyteam.db;
 
+import com.pyteam.db.mapper.TestMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,20 +9,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
-// @RunWith(SpringRunner.class)
-// @SpringBootTest
-// public class DbApplicationTests
-// {
-//
-//     @Autowired
-//     private TestMapper testMapper;
-//
-//     @Test
-//     @Rollback
-//     public void findByName() throws Exception
-//     {
-//         // testMapper.insert("BBB", 12);
-//         com.pyteam.db.mbg.entity.Test u = testMapper.findByName("BBB");
-//         Assert.assertEquals(12, u.getAge().intValue());
-//     }
-// }
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbApplicationTests
+{
+
+    @Autowired
+    private TestMapper testMapper;
+
+    @Test
+    @Rollback
+    public void findByName() throws Exception
+    {
+        testMapper.insert("BBB", 12);
+        com.pyteam.db.entity.Test u = testMapper.findByName("BBB");
+        Assert.assertEquals(12, u.getAge().intValue());
+    }
+}
