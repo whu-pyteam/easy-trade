19ba945aefb173485429efcefc1e597ae1f27575
tyc <952530904@qq,com>
2019-07-13 09:39:27 +0800

    fix: ad01表字段错误

--- db/src/main/java/com/pyteam/db/mbg/mapper/Ad02Mapper.java
+++ db/src/main/java/com/pyteam/db/mbg/mapper/Ad02Mapper.java
@@ -4,7 +4,9 @@ import com.pyteam.db.mbg.entity.Ad02;
 import com.pyteam.db.mbg.entity.Ad02Example;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Ad02Mapper {
     long countByExample(Ad02Example example);
 
--- db/src/main/java/com/pyteam/db/mbg/mapper/Af02Mapper.java
+++ db/src/main/java/com/pyteam/db/mbg/mapper/Af02Mapper.java
@@ -4,7 +4,9 @@ import com.pyteam.db.mbg.entity.Af02;
 import com.pyteam.db.mbg.entity.Af02Example;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Af02Mapper {
     long countByExample(Af02Example example);
 
