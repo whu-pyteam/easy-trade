ddd8f527fdc8de06767e351c0fecaa5e83215046
e <2361991194@qq.com>
2019-07-17 09:18:50 +0800

    添加拍卖商品收藏

--- foreground/src/main/java/com/pyteam/foreground/controller/AuctionController.java
+++ foreground/src/main/java/com/pyteam/foreground/controller/AuctionController.java
@@ -2,6 +2,7 @@ package com.pyteam.foreground.controller;
 
 import com.pyteam.foreground.dto.Ad02Dto;
 import com.pyteam.foreground.service.Ad02Service;
+import com.pyteam.foreground.service.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,82 +10,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.pyteam.foreground.controller.LoginController.isLogin;
+import static com.pyteam.foreground.controller.LoginController.getCookies;
+
 @Controller
 public class AuctionController
 {
     @Autowired
     private Ad02Service service;
 
+    @Autowired
+    private ConnectionService connService;
+
     @RequestMapping(value = "/auctionShow.html", method = RequestMethod.GET)
     public String searchById(int id, Model model)
     {
-        try
-        {
-            System.out.println(id);
-            model.addAttribute("ad02", service.findById(id));
-            model.addAttribute("type", 3);
-            return "auctionShow";
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return "error/404";
-        }
+        model.addAttribute("ad02", service.findById(id));
+        model.addAttribute("type", 3);
+        return "auctionShow";
     }
 
     @RequestMapping(value = "/auctionSearch.html", method = RequestMethod.POST)
     public String searchByValue(@RequestParam(value = "searchValue") String value, Model model)
     {
-        try
-        {
-            model.addAttribute("searchList", service.searchByValue(value));
-            model.addAttribute("type", 3);
-            return "auctionSearch";
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return "snow";
-        }
+        model.addAttribute("searchList", service.searchByValue(value));
+        model.addAttribute("type", 3);
+        return "auctionSearch";
     }
 
     @RequestMapping(value = "/auctionLaunch.html", method = RequestMethod.GET)
-    public String insert(Ad02Dto dto, Model model)
+    public String insert(Model model)
     {
         model.addAttribute("type", 3);
         return "auctionLaunch";
     }
 
     @RequestMapping(value = "/auctionLaunch.html", method = RequestMethod.POST)
-    public String add(Ad02Dto dto, Model model)
+    public String insert(Ad02Dto dto, Model model)
     {
-        try
-        {
-            model.addAttribute("type", 3);
-            service.addAd02(dto);
-            return "auctionLaunch";
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return "error/404";
-        }
+        model.addAttribute("type", 3);
+        service.insert(dto);
+        return "auctionLaunch";
     }
 
 
     @RequestMapping(value = "/auction.html", method = RequestMethod.GET)
-    public String wel(Model model)
+    public String wel(HttpServletRequest request, HttpServletResponse response, Model model)
     {
-        try
+        int aab101 = 0;
+        if(isLogin(request, response))
         {
-            model.addAttribute("ad02List", service.selectAll());
-            model.addAttribute("type", 3);
-            return "auction";
+            aab101 = Integer.parseInt(getCookies(request, "username"));
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return "snow";
-        }
+        model.addAttribute("aconnList", connService.selectAd02AndAe05(aab101, "2"));
+        //model.addAttribute("ad02List", service.selectAll());
+        model.addAttribute("type", 3);
+        return "auction";
     }
 }
