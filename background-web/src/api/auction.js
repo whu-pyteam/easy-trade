47eb29664254ed11ab5a1ba294f0e7d912adc7b9
tyc <952530904@qq,com>
2019-07-25 13:03:56 +0800

    fix

--- background-web/src/api/auction.js
+++ background-web/src/api/auction.js
@@ -1,10 +1,16 @@
 import request from "@/utils/request"
+import da from 'element-ui/src/locale/lang/da'
 
 export function getAuctionList(data) {
   return request({
     url: '/auction',
     method: 'post',
-    data
+    data: {
+      aad202: data.aad202,
+      aad209: data.aad209,
+      pageNum: data.pageNum,
+      pageSize: data.pageSize
+    }
   })
 
 }
